3f98a1b9da4daaa292265c787a8a9f5e5df849cc
Ana Carolina Junqueira e Silva <acjunqueiras@gmail.com>
2024-11-01 10:03:42 -0300

    adiciona e deleta propriedades

--- script.js
+++ script.js
@@ -11,31 +11,42 @@
 
 //console.log(sobre[1])
 
-let filmes = ["Interestelar", "Tropa de Elite", "Contato", "Jogo da Imitação", "o Alto da Compadecida"]
+//let filmes = ["Interestelar", "Tropa de Elite", "Contato", "Jogo da Imitação", "o Alto da Compadecida"]
 
-console.log(filmes)
+//console.log(filmes)
 
-filmes.push("Psicopata Americano")
-console.log(filmes)
+//filmes.push("Psicopata Americano")
+//console.log(filmes)
 
-filmes.unshift("O Circulo")
-console.log(filmes)
+//filmes.unshift("O Circulo")
+//console.log(filmes)
 
 //.pop <--- remove o último item da array
-filmes.pop()
-console.log(filmes)
+//filmes.pop()
+//console.log(filmes)
 
 //.shift <--- remove o primeiro item da array
-filmes.shift()
-console.log(filmes)
+//filmes.shift()
+//console.log(filmes)
 
 //---------- Objetos ----------
 
 //[] <--- Arrays
 //{} <--- Objetos
+
 const filme = {
     nome: "Corra",
     lancamento: 2017,
     tomatometer: 98,
     genero: "suspense"
 }
+
+console.log(filme.genero)
+
+//adiciona propriedade
+filme.bilheteria = 300
+console.log(filme)
+
+//deleta propriedade
+delete filme.avaliação
+console.log(filme)
